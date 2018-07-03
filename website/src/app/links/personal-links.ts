9122faeea842c0cce08e90bc0ba3e54eea055475
Harjuno Perwironegoro <hperwironegoro@gmail.com>
2018-07-04 00:51:29 +0100

    Adding social medi links to the top of the toolbar.

---
+++ website/src/app/links/personal-links.ts
@@ -0,0 +1,5 @@
+export const linkInstagram = 'https://www.instagram.com/unoperwironegoro/';
+export const linkLinkedIn = 'https://www.linkedin.com/in/unoper/';
+export const linkGitHub = 'https://github.com/unoperwironegoro';
+export const linkSoundcloud = 'https://soundcloud.com/uno-perwironegoro';
+export const linkTwitter = 'https://twitter.com/UnoPerwironegor';
--- website/src/app/nav-bar/nav-bar.component.ts
+++ website/src/app/nav-bar/nav-bar.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { linkInstagram, linkLinkedIn, linkGitHub, linkSoundcloud, linkTwitter } from '../links/personal-links';
 
 @Component({
   selector: 'app-nav-bar',
@@ -12,6 +13,14 @@ export class NavBarComponent {
   public menuItems: { link: string, name: string }[] = [
     { link: 'about', name: 'about'},
     { link: 'mystery', name: 'todo' }
+  ];
+
+  public toolbarLinks: { icon: string, link: string }[] = [
+    { icon: 'fab fa-lg fa-instagram', link: linkInstagram },
+    { icon: 'fab fa-lg fa-linkedin', link: linkLinkedIn },
+    { icon: 'fab fa-lg fa-github', link: linkGitHub },
+    { icon: 'fab fa-lg fa-soundcloud', link: linkSoundcloud },
+    { icon: 'fab fa-lg fa-twitter', link: linkTwitter }
   ];
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
